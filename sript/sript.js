6644f535dcf98869c9881c4394906e0e1744666d
subin <pc666666@naver.com>
2023-07-31 17:39:15 +0900

    max-width

--- sript/sript.js
+++ sript/sript.js
@@ -38,11 +38,12 @@ close.addEventListener('click',function(){
 const pawin_slide = new Swiper('#pawin_slide',{
     //자동재생
     autoplay:{
-        delay:2000,//슬라이드 간격(밀리초) 기본3초
+        delay:10000,//슬라이드 간격(밀리초) 기본3초
         disableOnInteraction:false,//버튼 클릭후 자동재생유지
     }, //자동재생
     loop:true, //마지막슬라이드 ->첫번째슬라이드 자연스러운 변경
-    effect:'fade', //제자리 자연스러운 슬라이드 효과
+    /* effect:'fade', */ //제자리 자연스러운 슬라이드 효과
+    direction: 'horizontal',
     navigation: { //이전, 다음 내비게이션 연결
         //next,prev 객체 연결 시 부모를 적으면 body 안에 있는 모든 swiper-next,prev를 인식하기 때문에 개별인식가능한 부모이름을 반드시 앞에 먼저 작성한다.
         nextEl: '#pawin_slide .swiper-button-next',
@@ -52,14 +53,13 @@ const pawin_slide = new Swiper('#pawin_slide',{
 });
 const bin_slide = new Swiper('#bin_slide',{
     autoplay: {
-        delay:1000, 
+        delay:10000, 
         disableOnInteraction:false, 
     },
-    loop:true,
-    effect:'fade',
+    loop:true,//마지막 슬라이드->첫번째슬라이드 자연스러운 변경
+    //effect:'fade', //제자리 변경 슬라이드
     navigation: {
         nextEl: '#bin_slide .swiper-button-next',
         prevEl: '#bin_slide .swiper-button-prev',
     },
-    direction:'vertical'
-})
+});
